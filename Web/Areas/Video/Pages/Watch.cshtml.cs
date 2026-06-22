e5b2c07c16c04d3488dc2e0e31d3cf0762862311
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2026-06-22 14:38:30 -0400

    Actualización

---
+++ Web/Areas/Video/Pages/Watch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Areas.Video.Pages
+{
+    public class WatchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
