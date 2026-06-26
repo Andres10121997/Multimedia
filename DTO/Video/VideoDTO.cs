83383e6e7eeef192d1835a103a6e936ceeb6d0ca
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2026-06-26 10:06:03 -0400

    Add DTO

---
+++ DTO/Video/VideoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DTO.Video
+{
+    public sealed record VideoDTO
+    {
+        public required string Title { get; init; }
+        public required string Description { get; init; }
+        public DateOnly CreationDate { get; init; }
+        public TimeOnly CreationTime { get; init; }
+    }
+}
